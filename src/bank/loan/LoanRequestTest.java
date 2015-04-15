8895aa5d5cbab80b20c7c4a6d1367a6b292b2624
Kevin Acevedo <kevin.g.acevedo@gmail.com>
2015-04-14 22:19:08 -0400

    LoanRequest Test Case

--- src/bank/loan/LoanRequestTest.java
+++ src/bank/loan/LoanRequestTest.java
@@ -8,7 +8,9 @@ public class LoanRequestTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		LoanRequest lR = new LoanRequest("test", 100);
+		assertEquals("amount loaned out is incorrect", 100, lR.getAmount(), 0.0000001);
+		assertEquals("description is incorrect", "test", lR.toString());
 	}
 
 }

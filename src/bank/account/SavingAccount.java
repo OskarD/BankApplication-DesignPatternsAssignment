42c823a90f27c779dbba6c5bc43fc38e03e23b1e
Oskar Danielsson <oskar@smsgrupp.se>
2015-04-15 23:46:38 -0400

    Added a $2 charge when withdrawing with a SavingAccount Updated class diagrams associated with this change

--- src/bank/account/SavingAccount.java
+++ src/bank/account/SavingAccount.java
@@ -1,5 +1,7 @@
 package bank.account;
 
+import bank.MessageHandler;
+
 
 public class SavingAccount extends Account {
 
@@ -9,5 +11,12 @@ public class SavingAccount extends Account {
 	
 	public SavingAccount(String ownerName, double amount) {
 		super(ownerName, amount);
+	}
+	
+	@Override
+	public void withdraw(double amount) throws BankAccountException {
+		MessageHandler.print("Withdrawing money with your savings account costs an extra $2");
+		
+		super.withdraw(amount + 2.0);
 	}
 }
